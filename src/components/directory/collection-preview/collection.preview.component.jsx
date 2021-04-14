67df652fbdeca3b9f87ba499c7229d1346ab66e2
Vishal <vishal.codedrill@gmail.com>
2021-04-14 10:19:31 +0530

    deploying the app

--- src/components/directory/collection-preview/collection.preview.component.jsx
+++ src/components/directory/collection-preview/collection.preview.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import collectionItem from '../../collection-item/collection-item.component'
+// import collectionItem from '../../collection-item/collection-item.component'
 
 const CollectionPreview = ({ title, items }) => {
 	return (
