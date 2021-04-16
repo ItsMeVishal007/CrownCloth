dfcf30d7adad0599afd9f7f0c293d263dc7ce15c
Vishal <vishal.codedrill@gmail.com>
2021-04-16 18:15:33 +0530

    added firebase user data store

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Route } from 'react-router-dom';
 import ShopComponent from './pages/shop/Shop.component';
 import Header from './components/Header/Header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-out/sign-in-sign-out.component';
-import {auth} from './firebase/firebase.utils'
+import {auth , createUserProfileDocument} from './firebase/firebase.utils'
 
 class App extends React.Component{
   constructor(){
@@ -15,14 +15,15 @@ class App extends React.Component{
     };
   }
 
-  unSubscribedFromAuth = null;
+  unSubscribedFromAuth = null; 
 
   componentDidMount(){
-    this.unSubscribedFromAuth = auth.onAuthStateChanged(user=>{
-      this.setState({
-        currentUser : user
-      })
-      console.log(user)
+    this.unSubscribedFromAuth = auth.onAuthStateChanged(async user =>{
+      // this.setState({
+      //   currentUser : user
+      // })
+      createUserProfileDocument(user);
+      // console.log(user)
     })
   }
 
